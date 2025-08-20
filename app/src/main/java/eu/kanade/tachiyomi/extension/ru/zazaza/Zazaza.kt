bd8a0bcf1d1fa5aeb075d2958c38a81ac8ed0fd8
Ambear1337 <delta38tema@gmail.com>
2025-08-20 13:17:31 +0500

    fix: add missing FilterList import

--- app/src/main/java/eu/kanade/tachiyomi/extension/ru/zazaza/Zazaza.kt
+++ app/src/main/java/eu/kanade/tachiyomi/extension/ru/zazaza/Zazaza.kt
@@ -4,6 +4,7 @@ import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
+import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
 import okhttp3.Headers
 import okhttp3.Request
